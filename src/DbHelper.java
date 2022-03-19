842f3f21a830212a31e8eaa3a15aadd10762b687
Muhammed Kürşat Gökgün <kgokgun94@gmail.com>
2022-03-19 23:47:43 +0300

    Transactions are finished.

---
+++ src/DbHelper.java
@@ -0,0 +1,40 @@
+
+import java.sql.*;
+
+
+public class DbHelper {
+    private String userName="root";
+    private String password="1234";
+    private String dbUrl="jdbc:mysql://localhost:3306/world?useSSL=false";//&serverTimezone=UTC
+    
+    public Connection getConnection()throws SQLException{
+        return DriverManager.getConnection(dbUrl,userName,password);
+    }
+    
+    public void showErrorMessage (SQLException exception){
+        System.out.println("Error: "+exception.getMessage());
+        System.out.println("Error code = "+exception.getErrorCode());
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
